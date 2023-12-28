387d37cb01f720ce47e688e52181648d21c736d6
Nicholas Wiebe <digitalexistence18@gmail.com>
2023-12-27 17:29:45 -0700

    Backup unsaved changes

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
-let balance = 500.00;
 
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
   }
+}
 
-  commit() {
-    balance -= this.amount;
-  }
+class Transaction {
+constructor(amount, account) {
+  this.amount = amount;
+    this.account = account;
+}
+
+commit() {
+  this.account.balance += this.value; //because of multiple instanaces we use a generic this.account to find the right balance
+}
 
 }
+
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
+}
+
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+
+   // get value() {
+   // return this.value;
+  }
+
+  }
 
 
 
@@ -18,12 +40,18 @@ class Withdrawal {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("snow-patrol");
+
+console.log('Starting Balance:', myAccount.balance);
+
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+t3 = new Deposit(120.0, myAccount);
+t3.commit();
+
+console.log('Ending Balance:', myAccount.balance);
+
